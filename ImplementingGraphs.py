eb9f13c3093362e5b5b966ab4113ff739e2b1ffb
T-Bone-Willson <32392016+T-Bone-Willson@users.noreply.github.com>
2019-03-11 15:02:06 +0000

    New graph tutorial
    trying new graph tutorial. Could not get results from the previous tutorials graph.

--- ImplementingGraphs.py
+++ ImplementingGraphs.py
@@ -2,9 +2,33 @@
 
 #This is a Directed Graph
 #Create Dictionary with elements within that represent "Nodes/Vertices"
-graph = {'A': ['B', 'C'], #"['B', 'C']" represents edge connection from Node A
-         'B': ['C', 'D'],
-         'C': ['D'],
-         'D': ['C'],
-         'E': ['F'],
-         'F': ['C']}
+#graph = {'A': ['B', 'C'], #"['B', 'C']" represents edge connection from Node A
+#         'B': ['C', 'D'], # "[]" connectable nodes are contained in a list
+#         'C': ['D'],
+#         'D': ['C'],
+#         'E': ['F'],
+#         'F': ['C']}
+
+#def find_path(graph, start, end, path=[]):
+#    path = path + [start]
+#    if start == end:
+#        return path
+#    if not graph.has_key(start):
+#        return None
+#    for node in graph[start]:
+#        if node not in path:
+#            newpath = find_path(graph, node, end, path)
+#            if newpath: return newpath
+#    return None
+
+
+# Create the dictionary with graph elements
+graph = {"a" : ["b", "c"],
+         "b" : ["a", "d"],
+         "c" : ["a", "d"],
+         "d" : ["e"],
+         "e" : ["d"]
+         }
+
+#Print graph
+print(graph)
