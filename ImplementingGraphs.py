f9b13b92450e341532f4e3569549cffa0ab0fdf3
T-Bone-Willson <32392016+T-Bone-Willson@users.noreply.github.com>
2019-03-11 14:34:50 +0000

    Dicitonary - Graph
    Created a Graph using a Dictionary. Filled with elements.

--- ImplementingGraphs.py
+++ ImplementingGraphs.py
@@ -1,1 +1,10 @@
-print("Hello World")
+#print("Hello World")
+
+#This is a Directed Graph
+#Create Dictionary with elements within that represent "Nodes/Vertices"
+graph = {'A': ['B', 'C'], #"['B', 'C']" represents edge connection from Node A
+         'B': ['C', 'D'],
+         'C': ['D'],
+         'D': ['C'],
+         'E': ['F'],
+         'F': ['C']}
